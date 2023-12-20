5a19fffa9073ea30b22e924ed867ce3968aee719
Yassien Abdillahi <yassien.abdillahi@tpximpact.com>
2023-12-20 14:09:15 +0000

    PDMED-6 and PDMED-7

--- pdmedcalc-v2-app/src/app/(home-page)/page.js
+++ pdmedcalc-v2-app/src/app/(home-page)/page.js
@@ -1,9 +1,11 @@
 import styles from './home-page.module.scss';
 import Explainer from '../components/explainer/Explainer';
+import TextBox from '../components/text-box/TextBox';
 export default function Home() {
   return (
     <main>
       <Explainer />
+      <TextBox />
     </main>
   )
 }
