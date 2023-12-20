2029486da4c0462dba4e6930bc593d3cb7a2541e
Yassien Abdillahi <yassien.abdillahi@tpximpact.com>
2023-12-20 15:26:19 +0000

    PDMED-13 and PDMED-20 Create calculator and results route

---
+++ pdmedcalc-v2-app/src/app/calculator/page.js
@@ -0,0 +1,7 @@
+export default function Calculator() {
+    return (
+        <main>
+            <p>You are now  on the calculator page</p>
+        </main>
+    )
+}
---
+++ pdmedcalc-v2-app/src/app/results/page.js
@@ -0,0 +1,7 @@
+export default function Results() {
+    return (
+        <main>
+            <p>You are now  on the results page</p>
+        </main>
+    )
+}
