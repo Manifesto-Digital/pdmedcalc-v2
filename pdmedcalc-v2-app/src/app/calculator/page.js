5662ca1be7f3a3733efe4f0e9e81136e42228cb3
Yassien Abdillahi <yassien.abdillahi@tpximpact.com>
2023-12-21 08:24:41 +0000

    PDMED-16 & PDMED-15 create back component and add it to calculator page

--- pdmedcalc-v2-app/src/app/calculator/page.js
+++ pdmedcalc-v2-app/src/app/calculator/page.js
@@ -1,7 +1,11 @@
+import styles from './calculator-page.module.scss'
+import Back from "../components/back/Back"
 export default function Calculator() {
     return (
         <main>
-            <p>You are now  on the calculator page</p>
+            <Back href='/' />
+            <h2 className={styles.heading}>Dose Calculator</h2>
+            <h3 className={styles.subheading}>Enter each medication and the frequency (per day)</h3>
         </main>
     )
 }
