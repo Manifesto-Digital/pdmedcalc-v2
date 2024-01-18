5fce7093c205cfda5925c47248b1e82a30dc5fc5
Yassien Abdillahi <yassien.abdillahi@tpximpact.com>
2024-01-18 14:20:19 +0000

    Minor styling fixes following review from UI/UX designer
    Adjust heading sizes on home page and alignment of select element on calculator page for desktops

--- src/app/components/explainer/Explainer.jsx
+++ src/app/components/explainer/Explainer.jsx
@@ -3,10 +3,10 @@ import styles from './explainer.module.scss'
 export default function Explainer() {
     return (
         <>
-            <h1 className={styles.heading}>PD 'Nil by Mouth' Medication Dose Calculator</h1>
-            <h2 className={styles.subheading}>Who is this tool for?</h2>
+            <h1 className={styles.heading + ' h2'}>PD 'Nil by Mouth' Medication Dose Calculator</h1>
+            <h2 className={styles.subheading + ' h3'}>Who is this tool for?</h2>
             <p className={styles.sentence}>Doctors, nurses and pharmacists who are looking after patients with Parkinson’s Disease (PD) who have been admitted to hospital and are unable to take their medications orally.</p>
-            <h2 className={styles.subheading}>What is the purpose of the tool?</h2>
+            <h2 className={styles.subheading + ' h3'}>What is the purpose of the tool?</h2>
             <ul className={styles.bullets}>
                 <li className={styles.sentence}><p>Suddenly stopping PD medications can be extremely dangerous and due to the risk of neuroleptic malignant syndrome, potentially even fatal.</p></li>
                 <li className={styles.sentence}><p>In patients unable to take their usual PD medications orally, this tool is designed to convert a patient’s usual PD medications to a ‘Levodopa equivalent dose’ (LED).</p></li>
@@ -15,7 +15,7 @@ export default function Explainer() {
                 <li className={styles.sentence}><p>The following medications can be safely omitted if swallow is compromised: selegiline, rasagiline, safinamide and amantadine.</p></li>
                 <li className={styles.sentence}><p>If the patient is on an Apomorphine infusion or uses Duodopa (via PEJ), then they do not need to be converted to a rotigotine patch – instead, please continue to use their existing non-oral treatment.</p></li>
             </ul>
-            <h2 className={styles.subheading}>Where did this tool come from?</h2>
+            <h2 className={styles.subheading + ' h3'}>Where did this tool come from?</h2>
             <ul className={styles.bullets}>
                 <li className={styles.sentence}><p>The development of the tool was led by James Fisher and the Northumbria Healthcare NHS Foundation Trust Parkinson’s Disease team in 2014, who worked in collaboration with Daniel Jamieson (Computing Science PhD student at Newcastle University).</p></li>
                 <li className={styles.sentence}><p>The tool was redeveloped in 2024 with support from TPXimpact and Parkinson’s UK.</p></li>
