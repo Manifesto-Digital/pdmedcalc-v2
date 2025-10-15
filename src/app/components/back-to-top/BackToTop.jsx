768cc90973f99156fbfde3c58c423bc0db88b01d
John Ennew <john.ennew@manifesto.co.uk>
2025-10-15 12:17:28 +0100

    Added a print styles sheet

--- src/app/components/back-to-top/BackToTop.jsx
+++ src/app/components/back-to-top/BackToTop.jsx
@@ -3,6 +3,6 @@ import styles from './back-to-top.module.scss'
 
 export default function BackToTop() {
     return (
-        <Link className={styles.link} href="#main-content">Back to top</Link>
+        <Link className={styles.link + ' back-to-top'} href="#main-content">Back to top</Link>
     )
 }
